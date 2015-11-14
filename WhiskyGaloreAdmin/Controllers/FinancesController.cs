cecc56faa72c4cd7fed260cc0eb199e8ef61a57a
Aistis <smikislt@yahoo.com>
2015-11-14 18:20:22 +0000

    Starting on finances

---
+++ WhiskyGaloreAdmin/Controllers/FinancesController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using WhiskyGaloreAdmin.Models;
+
+namespace WhiskyGaloreAdmin.Controllers
+{
+    public class FinancesController : Controller
+    {
+        // GET: Finances
+        public ActionResult DailyFinances()
+        {
+            return View(new Manager("getDailyFinances"));
+        }
+    }
+}

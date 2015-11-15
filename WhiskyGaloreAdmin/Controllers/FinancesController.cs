e2bbe57c1e7440ce1e5ede047b32aa84afc8737c
Aistis <smikislt@yahoo.cm>
2015-11-15 23:28:15 +0000

    Monthly/yearly dailyfinances

--- WhiskyGaloreAdmin/Controllers/FinancesController.cs
+++ WhiskyGaloreAdmin/Controllers/FinancesController.cs
@@ -23,5 +23,40 @@ namespace WhiskyGaloreAdmin.Controllers
             m.getData("getDailyFinances");
             return View("DailyFinances",m);
         }
+
+        [HttpGet]
+        public ActionResult MonthlyFinances()
+         {
+            Manager m = new Manager();
+            m.getData("getMonthlyFinances");
+            return View(m);
+        }
+        [HttpGet]
+        public ActionResult CalculateMonthly(int month,string year)
+        {
+            ModelState.Clear();
+            Manager m = new Manager();
+            m.calculateMonthlyFinances (month,year);
+            m.getData("getMonthlyFinances");
+            return View("MonthlyFinances", m);
+        }
+
+        [HttpGet]
+        public ActionResult YearlyFinances()
+        {
+            Manager m = new Manager();
+            m.getData("getYearlyFinances");
+            return View(m);
+        }
+
+        public ActionResult CalculateYearly(string year)
+        {
+            ModelState.Clear();
+            Manager m = new Manager();
+            m.calculateYearlyFinances(year);
+            m.getData("getYearlyFinances");
+            return View("YearlyFinances", m);
+        }
+
     }
 }
